ef3e2adc62832982c0776ea1dd31d21887c7e9d4
heba892 <hebagamal231199@gmail.com>
2023-08-27 23:50:01 +0300

    add zindex

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
   const date = new Date();
   return (
     <div className={`${animate ? 'animate-shake' : 'animate-none'} w-full h-screen bg-gradientBg bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-4 lg:px-0`}>
-    {errorMsg && (<div className='w-full max-w-[90vw] lg:max-w-[450px] bg-[rgb(255,32,32)] text-white absolute top-2 lg:top-5 p-1 capitalize rounded-md'>{`${errorMsg.response.data.message}`}</div>)}
+    {errorMsg && (<div className='w-full max-w-[90vw] lg:max-w-[450px] bg-[rgb(255,32,32)] text-white absolute top-2 lg:top-5 p-1 capitalize rounded-md z-40'>{`${errorMsg.response.data.message}`}</div>)}
       <form className='h-16 bg-black/30 w-full max-w-[450px] rounded-full backdrop-blur-[32px] mb-8'>
         <div className='h-full relative flex items-center justify-between p-2'>
           <input onChange={(e) => handleInput(e)} className='flex-1 bg-transparent outline-none placeholder:text-white text-white text-[15px] font-light pl-6 h-full' type='text' placeholder='Search by city or country'></input>
